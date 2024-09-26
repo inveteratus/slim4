2a8593f3532e65a497abe71d1719c9dc2b198e57
Inveteratus <alan.mcfarlane@gmail.com>
2024-09-26 08:42:10 +0100

    Removed redundant middleware

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -9,20 +9,18 @@ use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface;
 
 return function (App $app) {
-    $app->group('', function (RouteCollectorProxyInterface $app){
 
-        /* Guest only routes */
-        $app->group('', function (RouteCollectorProxyInterface $app) {
-            $app->get('/', IndexController::class)->setName('index');
-            $app->map(['GET', 'POST'], '/login', LoginController::class)->setName('login');
-            $app->map(['GET', 'POST'], '/register', RegisterController::class)->setName('register');
-        })->add($app->getContainer()->get('guest'));
+    /* Guest only routes */
+    $app->group('', function (RouteCollectorProxyInterface $app) {
+        $app->get('/', IndexController::class)->setName('index');
+        $app->map(['GET', 'POST'], '/login', LoginController::class)->setName('login');
+        $app->map(['GET', 'POST'], '/register', RegisterController::class)->setName('register');
+    })->add($app->getContainer()->get('guest'));
 
-        /* Authenticated only routes */
-        $app->group('', function (RouteCollectorProxyInterface $app) {
-            $app->post('/logout', LogoutController::class)->setName('logout');
-            $app->get('/home', HomeController::class)->setName('home');
-        })->add($app->getContainer()->get('auth'));
+    /* Authenticated only routes */
+    $app->group('', function (RouteCollectorProxyInterface $app) {
+        $app->post('/logout', LogoutController::class)->setName('logout');
+        $app->get('/home', HomeController::class)->setName('home');
+    })->add($app->getContainer()->get('auth'));
 
-    })->add('csrf');
 };
