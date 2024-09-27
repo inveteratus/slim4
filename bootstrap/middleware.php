671037a3844c40d6ab2b2e0b83388aa7cc9d7cb9
Inveteratus <alan.mcfarlane@gmail.com>
2024-09-27 06:01:45 +0100

    Changed order of middleware

--- bootstrap/middleware.php
+++ bootstrap/middleware.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 use Slim\App;
 
 return function (App $app) {
-    $app->add('csrf');
-    $app->add('twig');
-
     $app->addBodyParsingMiddleware();
     $app->addRoutingMiddleware();
 
+    $app->add('csrf');
+    $app->add('twig');
     $app->add('whoops');
 };
