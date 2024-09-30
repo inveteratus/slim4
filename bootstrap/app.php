0d210d195e2909f0c10a783c7a29416a3089fae5
Inveteratus <alan.mcfarlane@gmail.com>
2024-09-30 17:25:05 +0100

    Used macros for form elements

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,14 @@
 declare(strict_types=1);
 
 use DI\ContainerBuilder;
+use Respect\Validation\Factory;
 use Slim\App;
+
+Factory::setDefaultInstance(
+    (new Factory())
+        ->withRuleNamespace('App\\Validation\\Rules')
+        ->withExceptionNamespace('App\\Validation\\Exceptions'),
+);
 
 return (new ContainerBuilder())
     ->useAttributes(true)
--- public/index.php
+++ public/index.php
@@ -2,16 +2,7 @@
 
 declare(strict_types=1);
 
-use Respect\Validation\Factory;
-
 require __DIR__ . '/../vendor/autoload.php';
-
-/* Register our extra validation classes */
-Factory::setDefaultInstance(
-    (new Factory())
-        ->withRuleNamespace('App\\Validation\\Rules')
-        ->withExceptionNamespace('App\\Validation\\Exceptions'),
-);
 
 session_start();
 
